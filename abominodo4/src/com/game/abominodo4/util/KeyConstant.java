18e9be225e456b27a1da53950c5a98ddcb68a382
Dilki1997 <106859858+Dilki1997@users.noreply.github.com>
2022-06-16 19:01:33 +0530

    Update KeyConstant.java

--- abominodo4/src/com/game/abominodo4/util/KeyConstant.java
+++ abominodo4/src/com/game/abominodo4/util/KeyConstant.java
@@ -6,6 +6,6 @@ public class KeyConstant
 	
 	public static final String FILE_NAME_SCORE = "score.txt";
 	
-	public static final String J_PANEL_URI = "http://www.scit.wlv.ac.uk/~in6659/abominodo/"
+	public static final String J_PANEL_URI = "http://www.scit.wlv.ac.uk/~in6659/abominodo/";
 }
      
